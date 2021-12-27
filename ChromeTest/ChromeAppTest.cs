72132de1469f462681c7d34d38fe8807048a3770
Andrii Halyatovskyy <andry1294@gmail.com>
2021-12-27 15:23:24 +0200

    Fixed

--- ChromeTest/ChromeAppTest.cs
+++ ChromeTest/ChromeAppTest.cs
@@ -3,7 +3,7 @@
 namespace ClassLibrary1.ChromeTest
 {
     [TestFixture]
-    public class FirstTest : BaseTest
+    public class ChromeAppTest : BaseTestChrome
     {
         private string url = "https://www.google.com/";
         private string textToSearch = "Wikipedia";
