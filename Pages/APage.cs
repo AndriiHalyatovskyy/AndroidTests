31102a5efe8adf4dd243cb81480d21f319b725d7
Andrii Halyatovskyy <andry1294@gmail.com>
2022-09-13 17:12:42 +0300

    updated packages

--- Pages/APage.cs
+++ Pages/APage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VirtualDevice.Pages
+﻿namespace VirtualDevice.Pages
 {
     public abstract class APage<T>
     {
